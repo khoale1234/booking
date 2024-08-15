fae8278f5080a47ba75b74e75c3242fbfff4cb1e
khoale1234 <lengocdangkhoa03@gmail.com>
2024-08-15 10:09:00 +0700

    add AdminNewReservations

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -410,7 +410,16 @@ func (m *Repository) Logout(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 func (m *Repository) AdminDashBoard(w http.ResponseWriter, r *http.Request) {
-	render.Template(w, r, "admin-dashboard.page.tmpl", &models.TemplateData{})
+	reservations, err := m.DB.AllReservations()
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+	data := make(map[string]interface{})
+	data["reservations"] = reservations
+	render.Template(w, r, "admin-dashboard.page.tmpl", &models.TemplateData{
+		Data: data,
+	})
 }
 func (m *Repository) AdminNewReservations(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, r, "admin-new-reservations.page.tmpl", &models.TemplateData{})
